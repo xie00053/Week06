7e9f3c5d9df021ab520ce157da1b25840282a3d3
li xie <xie00053@algonquinlive.com>
2019-06-21 12:15:47 -0400

    Added a form and slots in the form component

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <slot name="title"></slot>
+        <form @submit.prevent="addTodo">
+            <input type="text" v-model="newTodo">
+        </form>
+        <slot name="desc"></slot>
+
+        <br/>
+        <slot>Default slot, placeholder content</slot>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            newTodo: ""
+        };
+    },
+    methods:{
+        addTodo(){
+            if(this.newTodo.length){
+                this.$emit('newTodo', this.newTodo);
+                this.newTodo = "";
+            }
+        }
+    }
+}
+</script>
+
+<style>
+input{
+    min-width: 200px;
+}
+</style>
+
+
--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,27 +1,53 @@
 <template>
     <div>
         <h1>Todo List</h1>
-        <Todo :todos="todolist" @removeTodo="appDeleteTodo"/>
+        <TodoForm @newTodo="addTodo">
+            <h2 slot="title">Add a Todo</h2>
+            <p slot="desc">Your Todos will be saved</p>
+            <h3>woshishui</h3>
+        </TodoForm>
+        <Todo :todos="todoList" @removeTodo="appDeleteTodo"/>
     </div>
 </template>
 
 <script>
 // 用 @ 可以直接进入要找的文件夹，不需要用 ../ 只类的方式
 import Todo from "@/components/Todo.vue";
+import TodoForm from "@/components/TodoForm.vue";
+
 export default {
     data(){
         return{
-            todolist:["Walk the dog", "Go for a ride"]
+            todoList:["Walk the dog", "Go for a ride"]
         };
     },
     components: {
-        Todo
+        Todo,
+        TodoForm
     },
     methods: {
         appDeleteTodo(index) {
             // 每次删除一个list
-            this.todolist.splice(index, 1);
+            this.todoList.splice(index, 1);
+        },
+        addTodo(todo){
+            this.todoList.push(todo);
         }
     }
 };
 </script>
+
+<style>
+ul{
+    list-style: none;
+    width: 50%;
+    margin: 0 auto;
+}
+
+ul li {
+        border-bottom:1px solid blue;
+        padding:10px 0;
+        margin-bottom: 10px;
+    }
+</style>
+
